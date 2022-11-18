438b78173b104a4a0c6e0ecd74edcc1355d584be
ldw01441929 <ldw01441929@alibaba-inc.com>
2022-11-18 14:14:17 +0800

    refine

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main(int, char **) {
   };
 
   auto rrange_to_string = [&](fpa::RRange<int> &rr) {
-    for (auto &r : rr) {
+    for (auto r : rr) {
       range_to_string(r);
     }
     std::cout << "\n";
@@ -26,7 +26,7 @@ int main(int, char **) {
 
   auto range2d_to_string = [&](fpa::Range2D<int> &rr, int width) {
     int i = 0;
-    for (auto &v : rr) {
+    for (auto v : rr) {
       std::cout << v.row << "." << v.col << " ";
       if (++i == width) {
         i = 0;
@@ -41,25 +41,25 @@ int main(int, char **) {
 
   auto rrange2d_to_string = [&](fpa::RRange2D<int> &rr2d) {
     int i = 0;
-    for (auto &r2d : rr2d) {
+    for (auto r2d : rr2d) {
       range2d_to_string(r2d, r2d.unit());
     }
     std::cout << "\n";
   };
 
   { // Range2D
-    int col = 20;
-    int row = 30;
-    fpa::Range2D<int> r2d(11, 11 + row, 55, 55 + col);
+    int col = 10;
+    int row = 10;
+    fpa::Range2D<int> r2d(0, 0 + row, 0, 0 + col);
     std::cout << "size: " << r2d.size() << ", is empty: " << std::boolalpha
               << r2d.empty() << std::endl;
     range2d_to_string(r2d, col);
   }
 
   { // RRange
-    int delta = 8;
-    int overlap = 3;
-    fpa::RRange<int> rr(14, 150, delta, overlap);
+    int delta = 4;
+    int overlap = 1;
+    fpa::RRange<int> rr(0, 10, delta, overlap);
 
     std::cout << "size: " << rr.size() << ", is empty: " << std::boolalpha
               << rr.empty() << std::endl;
@@ -67,15 +67,15 @@ int main(int, char **) {
   }
 
   { // RRange2D
-    int col = 32;
-    int row = 42;
-    int delta_row = 10;
-    int delta_col = 10;
-    int overlap_row = 1;
-    int overlap_col = 1;
+    int col = 20;
+    int row = 20;
+    int delta_row = 6;
+    int delta_col = 6;
+    int overlap_row = 2;
+    int overlap_col = 2;
 
-    fpa::RRange2D<int> rr2d(100, 100 + row, delta_row, overlap_row, 200,
-                            200 + col, delta_col, overlap_col);
+    fpa::RRange2D<int> rr2d(10, 10 + row, delta_row, overlap_row, 50, 50 + col,
+                            delta_col, overlap_col);
 
     std::cout << "size: " << rr2d.size() << ", is empty: " << std::boolalpha
               << rr2d.empty() << std::endl;
--- range.h
+++ range.h
@@ -46,28 +46,27 @@ public:
     return tmp;
   }
 
-  value_type &operator*() { return val_; }
+  value_type operator*() const { return val_; }
 
 private:
   value_type val_;
 };
 
 template <typename T> class Range {
-  friend class RRangeIterator<T>;
-  friend class RRange2DIterator<T, T>;
-
 public:
   using iterator = RangeIterator<T>;
+  using value_type = T;
 
 public:
-  Range() : begin_(static_cast<T>(0)), end_(static_cast<T>(0)) {}
-  Range(T begin, T end) : begin_(begin), end_(end) {}
+  Range()
+      : begin_(static_cast<value_type>(0)), end_(static_cast<value_type>(0)) {}
+  Range(value_type begin, value_type end) : begin_(begin), end_(end) {}
 
-  inline iterator cbegin() const { return empty() ? cend() : iterator(begin_); }
-  inline iterator begin() { return empty() ? end() : iterator(begin_); }
+  inline iterator begin() const { return empty() ? end() : iterator(begin_); }
+  inline typename iterator::value_type beginv() const { return *begin(); }
 
-  inline iterator cend() const { return iterator(end_); }
-  inline iterator end() { return iterator(end_); }
+  inline iterator end() const { return iterator(end_); }
+  inline typename iterator::value_type endv() const { return *end(); }
 
   inline typename iterator::difference_type size() const {
     return empty() ? 0 : end_ - begin_;
@@ -86,8 +85,8 @@ public:
   inline bool operator!=(const Range &rhs) const { return !(*this == rhs); }
 
 private:
-  T begin_;
-  T end_;
+  value_type begin_;
+  value_type end_;
 };
 
 template <typename TROW, typename TCOL = TROW> struct Range2DValue {
@@ -99,33 +98,33 @@ template <typename TROW, typename TCOL = TROW> class Range2DIterator {
 public:
   using iterator_category = std::forward_iterator_tag;
   using difference_type = std::size_t;
+  using row_type = Range<TROW>;
+  using col_type = Range<TCOL>;
   using value_type = Range2DValue<TROW, TCOL>;
   using reference = Range2DValue<TROW, TCOL> &;
   using pointer = Range2DValue<TROW, TCOL> *;
 
-  Range2DIterator(value_type val, const Range2D<TROW, TCOL> *container)
-      : val(val), container_(container) {}
+  Range2DIterator(row_type row, col_type col,
+                  typename row_type::iterator row_iter,
+                  typename col_type::iterator col_iter)
+      : row_(row), col_(col), row_iter_(row_iter), col_iter_(col_iter) {}
 
   bool operator==(const Range2DIterator &it) const { return !(*this != it); }
 
   bool operator!=(const Range2DIterator &it) const {
-    return val.col != it.val.col || val.row != it.val.row;
+    return row_ != it.row_ || col_ != it.col_ || row_iter_ != it.row_iter_ ||
+           col_iter_ != it.col_iter_;
   }
 
   Range2DIterator &operator++() {
-    typename Range<TROW>::iterator row_iter(val.row);
-    typename Range<TCOL>::iterator col_iter(val.col);
-    ++col_iter;
+    ++col_iter_;
     // next row
-    if (col_iter == container_->col_.cend()) {
+    if (col_iter_ == col_.end()) {
       // reset col index
-      col_iter = container_->col_.cbegin();
+      col_iter_ = col_.begin();
       // increment row index if not end of range2d
-      ++row_iter;
+      ++row_iter_;
     }
-
-    val.col = *col_iter;
-    val.row = *row_iter;
 
     return *this;
   }
@@ -136,39 +135,48 @@ public:
     return tmp;
   }
 
-  value_type &operator*() { return val; }
+  value_type operator*() const { return {*row_iter_, *col_iter_}; }
 
 private:
-  value_type val;
-  const Range2D<TROW, TCOL> *container_;
+  row_type row_;
+  col_type col_;
+  typename row_type::iterator row_iter_;
+  typename col_type::iterator col_iter_;
 };
 
 template <typename TROW, typename TCOL = TROW> class Range2D {
-  friend class RRange2DIterator<TROW, TCOL>;
-
 public:
   using iterator = Range2DIterator<TROW, TCOL>;
+  using row_type = Range<TROW>;
+  using col_type = Range<TCOL>;
 
 public:
   Range2D() {}
+  Range2D(row_type row, col_type col) : row_(row), col_(col) {}
   Range2D(TROW begin_row, TROW end_row, TCOL begin_col, TCOL end_col)
       : row_(begin_row, end_row), col_(begin_col, end_col) {}
 
-  inline iterator cbegin() const {
-    return empty() ? cend() : iterator({*row_.begin(), *col_.begin()}, this);
+  inline iterator begin() const {
+    return empty() ? end() : iterator(row_, col_, row_.begin(), col_.begin());
   }
-  inline iterator begin() {
-    return empty() ? end() : iterator({*row_.begin(), *col_.begin()}, this);
-  }
+  inline typename iterator::value_type beginv() const { return *begin(); }
 
-  inline iterator cend() const {
-    return iterator({*row_.end(), *col_.begin()}, this);
+  inline iterator end() const {
+    return iterator(row_, col_, row_.end(), col_.begin());
   }
-  inline iterator end() { return iterator({*row_.end(), *col_.begin()}, this); }
+  inline typename iterator::value_type endv() const { return *end(); }
 
+  inline typename row_type::iterator::difference_type rows() const {
+    return row_.size();
+  };
+  inline typename col_type::iterator::difference_type cols() const {
+    return col_.size();
+  };
   inline typename iterator::difference_type size() const {
     return row_.size() * col_.size();
   }
+  inline const row_type &row() const { return row_; };
+  inline const col_type &col() const { return col_; };
   inline bool empty() const { return row_.empty() || col_.empty(); }
 
   inline bool operator==(const Range2D &rhs) const { return !(*this != rhs); }
@@ -180,11 +188,8 @@ public:
   inline typename iterator::difference_type unit() { return col_.size(); }
 
 private:
-  Range<TROW> row_;
-  Range<TCOL> col_;
-
-private:
-  friend class Range2DIterator<TROW, TCOL>;
+  row_type row_;
+  col_type col_;
 };
 
 template <typename T> class RRangeIterator {
@@ -194,10 +199,11 @@ public:
   using value_type = Range<T>;
   using reference = Range<T> &;
   using pointer = Range<T> *;
+  using number_type = T;
 
   // 构造函数
-  RRangeIterator(T begin, T end, const RRange<T> *container)
-      : val_(begin, end), container_(container) {}
+  RRangeIterator(value_type val, value_type cap, number_type offset)
+      : val_(val), cap_(cap), offset_(offset) {}
 
   RRangeIterator<T> &operator=(const RRangeIterator &it) {
     val_ = it.val_;
@@ -207,19 +213,18 @@ public:
   bool operator!=(const RRangeIterator &it) const { return val_ != it.val_; }
 
   RRangeIterator &operator++() {
-    auto begin = val_.begin_;
-    auto end = val_.end_;
+    auto begin = val_.beginv();
+    auto end = val_.endv();
 
-    if (end == container_->end_) {
+    auto shrinked_size = std::min(offset_, cap_.endv() - val_.endv());
+    if (shrinked_size == 0) {
       // this is the last, the next is RRange::end()
-      (*this) = container_->cend();
+      // NOTE: this is the same as container::end()
+      val_ = Range<T>(cap_.endv(), cap_.endv());
     } else {
       //  the next is normal
-      auto offset = (container_->unit_ - container_->overlap_);
       // if the next is not a whole Range, shrink to fit size.
-      begin += offset;
-      end += std::min(offset, container_->end_ - end);
-      val_ = Range<T>(begin, end);
+      val_ = Range<T>(val_.beginv() + offset_, val_.endv() + shrinked_size);
     }
     return *this;
   }
@@ -230,61 +235,66 @@ public:
     return tmp;
   }
 
-  value_type &operator*() { return val_; }
+  value_type operator*() const { return val_; }
 
 private:
-  Range<T> val_;
-  const RRange<T> *container_;
+  value_type val_;
+  value_type cap_;
+  number_type offset_;
 };
 
+// RRange一个范围迭代器
+// 任意一个iterator的代表一个范围, 包含了连续的unit_个值
+// 最后一个有效的iterator(end()前面的那个)自适应范围
+// 任意相邻的两个iterator之间重叠overlap_个值
+// 举例
+// 假设overlap_=3, unit_=8, 当前RangeR: [-10, 15), 则:
+// begin iter: [-10, -2), unit_: 8
+// next1 iter: [-5, 3), unit_: 8, 与begin iter重叠了3个值: [-5, -4, -3]
+// next2 iter: [0, 8), unit_: 8, 与next1 iter重叠了3个值: [0, 1, 2]
+// next3 iter: [5, 13), unit_: 8, 与next2 iter重叠了3个值: [5, 6, 7]
+// next4 iter: [10, 15), unit_: 5, 与next3 iter重叠了3个值: [10, 11, 12]
+// 且next4 iter不是一个完整的unit_范围, 被缩减至适应范围了
+// end iter: [15, 15), unit_: 0, 不含有效范围
 template <typename T> class RRange {
-  friend class RRangeIterator<T>;
-  friend class RRange2D<T, T>;
-  friend class RRange2DIterator<T, T>;
-
 public:
   using iterator = RRangeIterator<T>;
+  using value_type = T;
 
 public:
   RRange()
-      : begin_(static_cast<T>(0)), end_(static_cast<T>(0)),
-        unit_(static_cast<T>(0)) {}
-  RRange(T begin, T end, T unit, T overlap)
+      : begin_(static_cast<value_type>(0)), end_(static_cast<value_type>(0)),
+        unit_(static_cast<value_type>(0)),
+        overlap_(static_cast<value_type>(0)) {}
+  RRange(value_type begin, value_type end, value_type unit, value_type overlap)
       : begin_(begin), end_(end), unit_(unit), overlap_(overlap) {}
 
-  inline iterator cbegin() const {
-    return empty() ? cend()
-                   : iterator(begin_, std::min(begin_ + unit_, end_), this);
+  inline iterator begin() const {
+    return empty()
+               ? end()
+               : iterator(typename iterator::value_type(begin_, begin_ + unit_),
+                          typename iterator::value_type(begin_, end_),
+                          unit_ - overlap_);
   }
-  inline iterator begin() {
-    return empty() ? end()
-                   : iterator(begin_, std::min(begin_ + unit_, end_), this);
-  }
+  inline typename iterator::value_type beginv() const { return *begin(); }
 
-  inline iterator cend() const { return iterator(end_, end_, this); }
-  inline iterator end() { return iterator(end_, end_, this); }
+  inline iterator end() const {
+    return iterator(typename iterator::value_type(end_, end_),
+                    typename iterator::value_type(end_, end_),
+                    unit_ - overlap_);
+  }
+  inline typename iterator::value_type endv() const { return *end(); }
 
   inline typename iterator::difference_type size() const {
-    return empty() ? 0 : std::distance(cbegin(), cend());
+    return std::distance(begin(), end());
   }
   inline bool empty() const { return begin_ >= end_; }
 
 private:
-  // 各个iterator的范围是unit_个值
-  // 各个iterator直接重叠overlap_个值
-  // 举例
-  // 假设overlap_=3, unit_=8, 当前RangeR: [-10, 15), 则:
-  // begin iter: [-10, -2), unit_: 8
-  // next1 iter: [-5, 3), unit_: 8, 与begin iter重叠了3个值: [-5, -4, -3]
-  // next2 Iter: [0, 8), unit_: 8, 与next1 iter重叠了3个值: [0, 1, 2]
-  // next3 Iter: [5, 13), unit_: 8, 与next2 iter重叠了3个值: [5, 6, 7]
-  // next4 Iter: [10, 15), unit_: 5, 与next3 iter重叠了3个值: [10, 11, 12]
-  // 且next4 iter不是一个完整的unit_范围, 被缩减至适应范围了
-  // end Iter: [15, 15), unit_: 0, 不含有效范围
-  T begin_;
-  T end_;
-  T unit_;
-  T overlap_;
+  value_type begin_;
+  value_type end_;
+  value_type unit_;
+  value_type overlap_;
 };
 
 template <typename TROW, typename TCOL = TROW> class RRange2DIterator {
@@ -294,48 +304,27 @@ public:
   using value_type = Range2D<TROW, TCOL>;
   using reference = Range2D<TROW, TCOL> &;
   using pointer = Range2D<TROW, TCOL> *;
+  using container_type = RRange2D<TROW, TCOL>;
+  using row_type = typename container_type::row_type;
+  using col_type = typename container_type::col_type;
 
-  // 构造函数
-  RRange2DIterator(TROW begin_row, TROW end_row, TCOL begin_col, TCOL end_col,
-                   const RRange2D<TROW, TCOL> *container)
-      : val(begin_row, end_row, begin_col, end_col), container_(container) {}
+  RRange2DIterator(row_type row, col_type col,
+                   typename row_type::iterator row_iter,
+                   typename col_type::iterator col_iter)
+      : row_(row), col_(col), row_iter_(row_iter), col_iter_(col_iter) {}
 
   bool operator==(const RRange2DIterator &it) const { return !(*this != it); }
 
-  bool operator!=(const RRange2DIterator &it) const { return val != it.val; }
+  bool operator!=(const RRange2DIterator &it) const {
+    return row_iter_ != it.row_iter_ || col_iter_ != it.col_iter_;
+  }
 
   RRange2DIterator &operator++() {
-    auto begin_row = val.row_.begin_;
-    auto end_row = val.row_.end_;
-
-    auto begin_col = val.col_.begin_;
-    auto end_col = val.col_.end_;
-
-    typename RRange<TROW>::iterator row_iter(begin_row, end_row,
-                                             &container_->row_);
-    typename RRange<TCOL>::iterator col_iter(begin_col, end_col,
-                                             &container_->col_);
-
-    if (end_row == container_->row_.end_ && end_col == container_->col_.end_) {
-      // this is the last, the next is RRange2D::end()
-      (*this) = container_->cend();
-    } else {
-      //  the next is normal
-
-      ++col_iter;
-
-      // next row
-      if (col_iter == container_->col_.cend()) {
-        // reset col index
-        col_iter = container_->col_.cbegin();
-        // increment row index if not end of rrange2d
-        ++row_iter;
-      }
-
-      val.row_ = *row_iter;
-      val.col_ = *col_iter;
+    ++col_iter_;
+    if (col_iter_ == col_.end()) {
+      col_iter_ = col_.begin();
+      ++row_iter_;
     }
-
     return *this;
   }
 
@@ -345,65 +334,58 @@ public:
     return tmp;
   }
 
-  value_type &operator*() { return val; }
+  value_type operator*() const {
+    return Range2D<TROW, TCOL>(*row_iter_, *col_iter_);
+  }
 
 private:
-  value_type val;
-  const RRange2D<TROW, TCOL> *container_;
-};
+  row_type row_;
+  col_type col_;
+  typename row_type::iterator row_iter_;
+  typename col_type::iterator col_iter_;
+}; // namespace fpa
 
 template <typename TROW, typename TCOL = TROW> class RRange2D {
-  friend class RRange2DIterator<TROW, TCOL>;
-
 public:
   using iterator = RRange2DIterator<TROW, TCOL>;
+  using row_type = RRange<TROW>;
+  using col_type = RRange<TCOL>;
 
 public:
   RRange2D() {}
+  RRange2D(const row_type &row, const col_type &col) : row_(row), col_(col) {}
   RRange2D(TROW begin_row, TROW end_row, TROW unit_row, TROW overlap_row,
            TCOL begin_col, TCOL end_col, TCOL unit_col, TCOL overlap_col)
       : row_(begin_row, end_row, unit_row, overlap_row),
         col_(begin_col, end_col, unit_col, overlap_col) {}
 
-  inline iterator cbegin() const {
-    return empty()
-               ? cend()
-               : iterator(row_.begin_,
-                          std::min(row_.begin_ + row_.unit_, row_.end_),
-                          col_.begin_,
-                          std::min(col_.begin_ + col_.unit_, col_.end_), this);
+  inline iterator begin() const {
+    return empty() ? end() : iterator(row_, col_, row_.begin(), col_.begin());
   }
-  inline iterator begin() {
-    return empty()
-               ? end()
-               : iterator(row_.begin_,
-                          std::min(row_.begin_ + row_.unit_, row_.end_),
-                          col_.begin_,
-                          std::min(col_.begin_ + col_.unit_, col_.end_), this);
-  }
+  inline typename iterator::value_type beginv() const { return *begin(); }
 
-  inline iterator cend() const {
-    return iterator(row_.end_, row_.end_, col_.end_, col_.end_, this);
+  inline iterator end() const {
+    return iterator(row_, col_, row_.end(), col_.begin());
   }
-  inline iterator end() {
-    return iterator(row_.end_, row_.end_, col_.end_, col_.end_, this);
-  }
+  inline typename iterator::value_type endv() const { return *end(); }
 
+  inline typename row_type::iterator::difference_type rows() const {
+    return row_.size();
+  };
+  inline typename col_type::iterator::difference_type cols() const {
+    return col_.size();
+  };
   inline typename iterator::difference_type size() const {
-    return std::distance(cbegin(), cend());
+    return row_.size() * col_.size();
   }
   inline bool empty() const { return row_.empty() || col_.empty(); }
 
-  // inline bool operator==(const RRange2D &rhs) const { return !(*this != rhs);
-  // }
-
-  // inline bool operator!=(const RRange2D &rhs) const {
-  //   return row_ != rhs.row_ || col_ != rhs.col_;
-  // }
+  inline const row_type &row() const { return row_; };
+  inline const col_type &col() const { return col_; };
 
 private:
-  RRange<TROW> row_;
-  RRange<TCOL> col_;
+  row_type row_;
+  col_type col_;
 };
 } // namespace fpa
 
